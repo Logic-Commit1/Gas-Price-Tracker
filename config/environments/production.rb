e2d7af1715f4172682c97b5dddbe9dd194d00070
Carl Rosales <rosales.carlangel@gmail.com>
2023-02-23 00:49:26 +0800

    Renamed app to Hypermilers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "logic_gas_price_production"
+  # config.active_job.queue_name_prefix = "hypermilers_production"
 
   config.action_mailer.perform_caching = false
 
